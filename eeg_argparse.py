01b309e26dafeb2a7b0fbb8638cd922d7692741a
cconaway <cconaway@ucsd.edu>
2022-08-16 09:49:39 -0700

    added client seperating

--- eeg_argparse.py
+++ eeg_argparse.py
@@ -1,16 +1,17 @@
 """This is essential for having two run."""
 
 import argparse
-from server_constants import DEFAULT_IP, DEFAULT_PORT
+from server_constants import DEFAULT_IP, DEFAULT_PORT, DEFAULT_CLIENT_PORT
 
 class EEG_argparse(object):
 
     def __init__(self):
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('-ip','--SERVER_IP', type=str, required=False)
-        self.parser.add_argument('-p','--SERVER_PORT', type=int, required=False)
+        self.parser.add_argument('-serverport','--SERVER_PORT', type=int, required=False)
         self.parser.add_argument('-prefix','--msg_prefix', type=str, required=False)
-    
+        self.parser.add_argument('-clientport', '--CLIENT_PORT', type=int, required=False)
+
     def run_parser(self):
         args = self.parser.parse_args()
         
@@ -20,16 +21,21 @@ class EEG_argparse(object):
             server_ip = args.SERVER_IP
 
         if args.SERVER_PORT == None:
-                server_port = DEFAULT_PORT
+            server_port = DEFAULT_PORT
         else:
             server_port = args.SERVER_PORT
+
+        if args.CLIENT_PORT == None:
+            client_port = DEFAULT_CLIENT_PORT
+        else:
+            client_port = args.CLIENT_PORT
 
         if args.msg_prefix == None:
             msg_prefix = None
         else:
             msg_prefix = args.msg_prefix
         
-        return server_ip, server_port, msg_prefix
+        return server_ip, server_port, client_port, msg_prefix
 
 
         
--- main.py
+++ main.py
@@ -5,26 +5,26 @@ import sys
 from pythonosc import osc_server
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.udp_client import SimpleUDPClient
-from .eeg_proc import SplitWaveHandler
 
 #Internal
-from eeg_proc import MotionHandler, RawEEGHandler, WaveHandler
+from eeg_proc import MotionHandler, RawEEGHandler, WaveHandler, SplitWaveHandler
 from range_parameters import ACC_INPUT, ACC_OUTPUT, GYRO_INPUT, GYRO_OUTPUT, ALLWAVE_INPUT, ALLWAVE_OUPUT
 from eeg_argparse import EEG_argparse
 
 
 def main():
     parser = EEG_argparse()
-    server_ip, server_port, msg_prefix = parser.run_parser() #Check Constants for Defaults
+    server_ip, server_port, client_port, msg_prefix = parser.run_parser() #Check Constants for Defaults
 
     #Fill with clients if you want
-    clients = [SimpleUDPClient('127.0.0.1', 8001), SimpleUDPClient('192.168.1.124', 8000)]
+    clients = [SimpleUDPClient('127.0.0.1', client_port)]
+    """Input method for making clients dynamically"""
 
     #######################################################
     dispatch = Dispatcher()
     
     #Accelerometer
-    acc = MotionHandler(input_range=ACC_INPUT, output_range=ACC_OUTPUT, window=2, msg_prefix=msg_prefix)
+    acc = MotionHandler(input_range=ACC_INPUT, output_range=ACC_OUTPUT, msg_prefix=msg_prefix)
     dispatch.map("/muse/acc", acc.run, clients)
 
     #Gyroscope
@@ -47,8 +47,9 @@ def main():
         dispatch.map(f"/muse/elements/{wave[0]}", wavehandler.run, clients, wave[1])
 
     #Sensor Split Alpha
+    '''
     alphasplit = SplitWaveHandler(wavename='alpha', input_range=ALLWAVE_INPUT, output_range=ALLWAVE_OUPUT, msg_prefix=msg_prefix)
-    dispatch.map("/muse/elements/alpha_absolute", alphasplit.run())
+    dispatch.map("/muse/elements/alpha_absolute", alphasplit.run())'''
     #########################################################
 
     server = osc_server.ThreadingOSCUDPServer((server_ip, server_port), dispatch)
